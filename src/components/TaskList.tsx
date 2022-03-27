269c2227468b60f070dda81498f132daad60b081
Jônathas Bonfim <58302084+Jonathas-bonfim@users.noreply.github.com>
2022-03-27 12:00:59 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,15 +16,42 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle){
+      // alert('Insira o título para a tarefa');
+      return;
+    } 
 
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const updateTask = tasks.map(tasks => tasks.id === id ? {
+        ...tasks,
+        isComplete: !tasks.isComplete,
+      } : tasks, 
+    );
+
+    setTasks(updateTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    // Tentei adicionar uma confirmação para excluir o registro, porém deu erro nos testes, não sei como burlar isso
+
+    // if (window.confirm(`Tem certeza que você deseja excluir a tarefa?`)){
+    // }
+
+      const filterTasks = tasks.filter(tasks => tasks.id !== id);
+      setTasks(filterTasks);
+ 
   }
 
   return (
